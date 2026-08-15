9a728a662be0d233998d9805d915d8084ef5ef0b
farhankhan011 <farhank023102@gmail.com>
2026-08-16 00:28:56 +0500

    Intro Completed

--- PandasStarted.py
+++ PandasStarted.py
@@ -4,8 +4,15 @@
 Pandas is a Python library.
 
 Pandas is used to analyze data.
+
+
+Learning by Reading
+ you can use the Pandas module, and modify the code to see the result.
+
 '''
+# Load a CSV file into a Pandas DataFrame:    just an example code 
+# import pandas as pd
+# df = pd.read_csv('data.csv')
+# print(df.to_string()) 
 
-
-
-
+# -----------INtro completed this much intro is enough to start learning it --------------
