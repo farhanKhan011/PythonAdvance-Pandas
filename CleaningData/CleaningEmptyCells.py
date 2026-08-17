82e64fe6f66be80ae0751abde757b2b3866177b8
farhankhan011 <farhank023102@gmail.com>
2026-08-18 01:18:11 +0500

    Cleaning Empty Cells lesson completed with mean() mode() median() , also update the csv file

--- CleaningData/CleaningEmptyCells.py
+++ CleaningData/CleaningEmptyCells.py
@@ -61,5 +61,34 @@ A common way to replace empty cells, is to calculate the mean, median or mode va
 Pandas uses the mean() median() and mode() methods to calculate the respective values for a specified column:
 
 Example
-Calculate the MEAN, and replace any empty values with it:
 '''
+# Calculate the MEAN, and replace any empty values with it:
+# import pandas as pd
+
+# df = pd.read_csv('CleaningData/data.csv')
+# x = df["Calories"].mean()
+# df.fillna({"Calories":x}, inplace = True)
+# print(df.to_string())
+# Mean = the average value (the sum of all values divided by number of values).
+
+''' 
+Calculate the MEDIAN, and replace any empty values with it: 
+'''
+# import pandas as pd
+# df = pd.read_csv("CleaningData/data.csv")
+# x = df["Calories"].median()
+# df.fillna({"Calories":x} , inplace = True)
+# print(df.to_string())
+# Median = the value in the middle, after you have sorted all values ascending.
+
+'''
+Mode = the value that appears most frequently.
+Calculate the MODE, and replace any empty values with it:
+'''
+# import pandas as pd
+# df = pd.read_csv("CleaningData/data.csv")
+# x = df["Calories"].mode()[0]
+# df.fillna({"Calories":x}, inplace = True)
+# print(df.to_string())
+
+# -----CleaningEmptyCells Completed--------
